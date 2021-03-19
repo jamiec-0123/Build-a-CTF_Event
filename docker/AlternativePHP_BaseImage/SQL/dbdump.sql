48db4e54eed4694e5258f4838c9dc7a52e2b16d2
markf11 <55436931+markf11@users.noreply.github.com>
2021-03-19 19:12:53 +0000

    Update dbdump.sql
    add more users

--- docker/AlternativePHP_BaseImage/SQL/dbdump.sql
+++ docker/AlternativePHP_BaseImage/SQL/dbdump.sql
@@ -5,4 +5,11 @@ Password varchar(255),
 );
 
 INSERT INTO Users (Username, Password)
-VALUES (John, 6R5t768y7E654uyg);
+VALUES 
+  (John, 6R5t768y7E654uyg)
+  (Rick_Astley, NeverGonnaGiveYouUp)
+  (Baby, Shark)
+  (Harry, IamaWizzard!)
+  (Cool_Guy, Neverseenexplonisons)
+  (Dan, Isalegend)
+  (Adam, Isalegend2);
